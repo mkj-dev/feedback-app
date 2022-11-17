af42db1a9384f780fdb24c64695087f6614031fd
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-11-17 09:48:15 +0100

    Add header and footer components

---
+++ incl/footer.php
@@ -0,0 +1,7 @@
+</main>
+
+<footer class="py-3 mt-4 bg-dark">
+    <p class="my-0 text-center text-white">&copy; 2022 Company, Inc</p>
+</footer>
+</body>
+</html>
---
+++ incl/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback App</title>
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar bg-dark">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1 text-white">Feedback App</span>
+        <ul class="navbar-nav flex-row">
+            <li class="nav-item mx-4">
+                <a class="nav-link text-white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item mx-4">
+                <a class="nav-link text-white" href="feedback.php">Feedback</a>
+            </li>
+            <li class="nav-item mx-4">
+                <a class="nav-link text-white" href="about.php">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<main class="container-md d-flex flex-column align-items-center">
