6b60483c29a27a869111a25358025fd8ec2983a9
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-11-17 13:39:56 +0100

    Loop through user feedback data and display it on the page

--- feedback.php
+++ feedback.php
@@ -1,27 +1,21 @@
 <?php include "incl/header.php" ?>
 
+<?php 
+$json = file_get_contents("./feedback_data/data.json");
+$data = json_decode($json, true);
+?>
+
 <h2 class="mt-5 mb-3">Past Feedback</h2>
-<div class="card w-50 my-3">
-    <div class="card-body">
-        <h5 class="card-title">John Doe</h5>
-        <p class="card-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo recusandae pariatur doloribus impedit sit assumenda tempora molestiae iusto!
-        </p>
-        <p class="card-text">
-            <small class="text-muted">Last updated 2 mins ago</small>
-        </p>
+
+<?php foreach($data as $user): ?>
+    <div class="card w-50 my-3">
+        <div class="card-body">
+            <h5 class="card-title mb-0"><?= $user['name'] ?></h5>
+            <p class="card-text"><small class="text-muted"><?= $user['email'] ?></small></p>
+            <p class="card-text"><?= $user['feedback'] ?></p>
+            <p class="card-text"><small class="text-muted"><?= $user['feedback_date'] ?></small></p>
+        </div>
     </div>
-</div>
-<div class="card w-50 my-3">
-    <div class="card-body">
-        <h5 class="card-title">Jane James</h5>
-        <p class="card-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro magnam voluptatum illo aspernatur doloremque? Nisi reiciendis vitae enim excepturi ipsum assumenda optio saepe voluptas dolor.
-        </p>
-        <p class="card-text">
-            <small class="text-muted">Added 20/07/2022</small>
-        </p>
-    </div>
-</div>
+<?php endforeach; ?>
 
 <?php include "incl/footer.php" ?>
