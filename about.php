a9caf39490eedc6d052bfe2fbcf4c2f45fab812f
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-11-17 09:48:54 +0100

    Include header and footer components

--- about.php
+++ about.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback App</title>
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
-    <main class="container-md d-flex flex-column align-items-center">
-        <h2 class="mt-5 mb-2">About</h2>
-        <div class="container-sm w-50 p-3">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, explicabo a animi deleniti ab officia quibusdam error porro aspernatur necessitatibus excepturi laborum aut ex perferendis beatae. Ipsam enim aliquid tenetur commodi provident earum doloribus ad adipisci possimus facilis voluptatibus reprehenderit velit eaque nulla magni delectus numquam, ipsum laborum unde sed!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, dolores tempora? Perferendis nihil accusamus quia facere placeat ut sed amet ex incidunt ab, repellendus necessitatibus quibusdam quis, consectetur animi asperiores.</p>
-        </div>
-    </main>
-</body>
-</html>
+<?php include "incl/header.php" ?>
+
+<h2 class="mt-5 mb-2">About</h2>
+<div class="container-sm min-vh-100 w-50 p-3">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, explicabo a animi deleniti ab officia quibusdam error porro aspernatur necessitatibus excepturi laborum aut ex perferendis beatae. Ipsam enim aliquid tenetur commodi provident earum doloribus ad adipisci possimus facilis voluptatibus reprehenderit velit eaque nulla magni delectus numquam, ipsum laborum unde sed!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, dolores tempora? Perferendis nihil accusamus quia facere placeat ut sed amet ex incidunt ab, repellendus necessitatibus quibusdam quis, consectetur animi asperiores.</p>
+</div>
+
+<?php include "incl/footer.php" ?>
--- index.php
+++ index.php
@@ -1,57 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback App</title>
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
-    <nav class="navbar bg-dark">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1 text-white">Feedback App</span>
-            <ul class="navbar-nav flex-row">
-                <li class="nav-item mx-4">
-                    <a class="nav-link text-white" href="index.php">Home</a>
-                </li>
-                <li class="nav-item mx-4">
-                    <a class="nav-link text-white" href="feedback.php">Feedback</a>
-                </li>
-                <li class="nav-item mx-4">
-                    <a class="nav-link text-white" href="about.php">About</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+<?php include "incl/header.php" ?>
 
-    <main class="container-md d-flex flex-column align-items-center">
-        <h2 class="mt-5 mb-2">Feedback</h2>
-        <p>Give us some super cool and positive feedback</p>
+<h2 class="mt-5 mb-2">Feedback</h2>
+<p>Give us some super cool and positive feedback</p>
 
-        <form action="" method="post" class="my-4">
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-            </div>
-              <div class="mb-3">
-                <label for="textarea" class="form-label">Feedback</label>
-                <textarea class="form-control" id="textarea" rows="3" placeholder="Enter your feedback..."></textarea>
-            </div>
-            <div class="mb-3">
-                <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
-            </div>
-        </form>
-    </main>
+<form action="" method="post" class="my-4">
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+    </div>
+        <div class="mb-3">
+        <label for="textarea" class="form-label">Feedback</label>
+        <textarea class="form-control" id="textarea" rows="3" placeholder="Enter your feedback..."></textarea>
+    </div>
+    <div class="mb-3">
+        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+    </div>
+</form>
 
-    <footer class="py-3 mt-4 bg-dark">
-        <p class="my-0 text-center text-white">&copy; 2022 Company, Inc</p>
-    </footer>
-</body>
-</html>
+<?php include "incl/footer.php" ?>
