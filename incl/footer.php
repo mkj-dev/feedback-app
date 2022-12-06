e303d624d1363d0c5224f37d2ebaefe2610d42f2
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-12-06 23:29:20 +0100

    Change footer info

--- incl/footer.php
+++ incl/footer.php
@@ -1,7 +1,7 @@
 </main>
 
 <footer class="py-3 mt-4 bg-dark">
-    <p class="my-0 text-center text-white">&copy; 2022 Company, Inc</p>
+    <p class="my-0 text-center text-white">&copy; 2022 github.com/mkj-dev</p>
 </footer>
 </body>
 </html>
