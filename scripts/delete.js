54362fb67561d124838f1ec106e09d6563d9aa20
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-11 21:48:14 +0100

    Fetch the delete.php script and pass it the id of the feedback card to delete

--- scripts/delete.js
+++ scripts/delete.js
@@ -2,9 +2,27 @@ const trashIcon = document.querySelectorAll('.bi-trash3-fill');
 
 // Delete the clicked feedback
 trashIcon.forEach(icon => {
-    const offsetParent = icon.parentNode.offsetParent
+    const feedbackCard = icon.parentNode.offsetParent
 
     icon.addEventListener('click', () => {
-        offsetParent.parentNode.removeChild(offsetParent);
+        const cardId = feedbackCard.getAttribute('data-id');
+        const id = cardId.split('-')[1];
+
+        fetch(`./delete.php`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `id=${id}`,
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            feedbackCard.parentNode.removeChild(feedbackCard);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
     });
 })
