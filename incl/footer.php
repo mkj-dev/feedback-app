7d5c372ecda0eda1b13eb478d88c3dbc6b4d3cc7
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-11 17:06:01 +0100

    Add github link

--- incl/footer.php
+++ incl/footer.php
@@ -1,7 +1,7 @@
 </main>
 
 <footer class="py-3 mt-4 bg-dark">
-    <p class="my-0 text-center text-white">&copy; 2022 github.com/mkj-dev</p>
+    <p class="my-0 text-center text-white">Copyright &copy; 2023 <a href="https://github.com/mkj-dev">github.com/mkj-dev</a></p>
 </footer>
 </body>
 </html>
