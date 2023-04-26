f4ff42bcf465c46cf799d4d0e32e8a9fada44a76
mkj-dev <56467077+mkj-dev@users.noreply.github.com>
2023-04-26 07:37:33 +0000

    Fix 'Cannot modify header information - headers already sent by (output started at /home/runner/feedback-app/incl/header.php:1) in /home/runner/feedback-app/index.php on line 13' error message, Change name, id and label from 'textarea' to 'feedback' in the form.

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php include "incl/header.php" ?>
-
 <?php
-if(!empty($_POST)){
-    $user = [];
-    $user['name'] = htmlentities($_POST['name']);
-    $user['email'] = htmlentities($_POST['email']);
-    $user['feedback'] = htmlentities($_POST['textarea']);
-    $user['feedback_date'] = date('D, d M Y H:i:s', time());
-    $userArray = json_decode(file_get_contents('./feedback_data/data.json'), true);
-    array_push($userArray, $user);
-    file_put_contents('./feedback_data/data.json', json_encode($userArray, JSON_PRETTY_PRINT));
-    header('Location: ./feedback.php');
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback = $_POST['feedback'];
+    $feedback_date = date("F j, Y, g:i a");
+    $new_feedback = array(
+        'name' => $name,
+        'email' => $email,
+        'feedback' => $feedback,
+        'feedback_date' => $feedback_date
+    );
+    $json = file_get_contents("./feedback_data/data.json");
+    $data = json_decode($json, true);
+    array_push($data, $new_feedback);
+    $json = json_encode($data);
+    file_put_contents("./feedback_data/data.json", $json);
+    header("Location: feedback.php");
+    exit;
 }
 ?>
+<?php include "incl/header.php" ?>
 <h2 class="mt-5 mb-2">Feedback</h2>
 <p>Give us some super cool and positive feedback</p>
 
@@ -26,8 +34,8 @@ if(!empty($_POST)){
         <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
     </div>
         <div class="mb-3">
-        <label for="textarea" class="form-label">Feedback</label>
-        <textarea class="form-control" name="textarea" id="textarea" rows="5" placeholder="Enter your feedback..." required></textarea>
+        <label for="feedback" class="form-label">Feedback</label>
+        <textarea class="form-control" name="feedback" id="feedback" rows="5" placeholder="Enter your feedback..." required></textarea>
     </div>
     <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
